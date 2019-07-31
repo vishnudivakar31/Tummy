7038350efab77ba00c01c91fcb6759f8be08b534
vishnudivakar31 <vishnudivakar31@gmail.com>
2019-07-31 15:40:54 +0530

    security bugs fixed.

--- spring-boot-app/src/main/java/com/wanderingThinker/Tummy/services/UserDetailsServiceImpl.java
+++ spring-boot-app/src/main/java/com/wanderingThinker/Tummy/services/UserDetailsServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private TummyUserRepository tummyUserRepository;
@@ -23,6 +25,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         if(user == null) {
             throw new UsernameNotFoundException(username);
         }
-        return new User(user.getUserName(), user.getPassword(), Collections.emptyList());
+        return new User(user.getUsername(), user.getPassword(), Collections.emptyList());
     }
 }
