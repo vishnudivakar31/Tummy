7f04299cf4cc43dce41f48c187da3601fa54e253
vishnudivakar31 <vishnudivakar31@gmail.com>
2019-08-11 22:28:22 +0530

    navigation initial commit

--- android-app/Tummy/app/src/main/java/com/wanderingthinkter/tummy/HomeActivity.kt
+++ android-app/Tummy/app/src/main/java/com/wanderingthinkter/tummy/HomeActivity.kt
@@ -3,23 +3,29 @@ package com.wanderingthinkter.tummy
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+                this.supportActionBar?.setTitle(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+            R.id.navigation_recipes -> {
+                this.supportActionBar?.setTitle(R.string.title_recipe)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+                this.supportActionBar?.setTitle(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_friends -> {
+                this.supportActionBar?.setTitle(R.string.title_friends_circle)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_account -> {
+                this.supportActionBar?.setTitle(R.string.title_account)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,10 +34,12 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        this.supportActionBar?.setTitle(R.string.title_home)
+
         setContentView(R.layout.activity_home)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
